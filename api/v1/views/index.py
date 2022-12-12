9440c367cb43548d4c0f10be9634327d4f7d53e0
m1dnas <felipesandimguliermone@gmail.com>
2022-12-11 23:51:19 -0300

    task 5 v1

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""Index file"""
-
-
+"""Index"""
 from api.v1.views import app_views
 import json
-
-
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 @app_views.route('/status')
 def status():
     return {"status": "OK"}
+
+@app_views.route('/stats')
+def stats():
+    return {"amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)}
